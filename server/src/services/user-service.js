b7fed1e63db94e31df450e57676193b6d0f62b0d
a-mo1kov <user.amo1kov07@gmail.com>
2026-02-04 19:58:12 +0300

    feat(auth): add UserService with signup and login business logic

---
+++ server/src/services/user-service.js
@@ -0,0 +1,44 @@
+import UserDatabase from '../data-access-layer/user-database.js';
+import Bcrypt from '../dependencies/bcrypt.js';
+import ApiError from '../utils/api-error.js';
+import generateToken from '../dependencies/jsonwebtoken.js';
+
+/*
+Service layer for handling complex business logic for users.
+*/
+class UserService {
+	//Processes user registration logic.
+	async serviceSignup(username, password) {
+		const user = await UserDatabase.isUsernameExists(username);
+		if (user.rows.length > 0) {
+			throw new ApiError(
+				409,
+				'This username is already taken, please choose another one.',
+			);
+		}
+
+		const passwordHash = await Bcrypt.generateHashPassword(password);
+
+		await UserDatabase.createUser(username, passwordHash);
+	}
+
+	//Processes user authentication and issues a token.
+	async serviceLogin(username, password) {
+		const userRequest = await UserDatabase.findUser(username);
+		if (userRequest.rows.length === 0) {
+			throw new ApiError(404, 'Person not found in the system.');
+		}
+
+		const user = userRequest.rows[0];
+
+		const validPassword = await Bcrypt.isValidPassword(password, user.password_hash);
+
+		if (!validPassword) {
+			throw new ApiError(400, 'Incorrect data.');
+		}
+
+		return generateToken(user.id, user.username);
+	}
+}
+
+export default new UserService();
