59e2b30f13ec8efcc22de02d74c5b518db21ffc6
a-mo1kov <user.amo1kov07@gmail.com>
2026-02-06 13:16:14 +0300

    feat: add signup page with validation

---
+++ client/src/pages/Auth/Signup.jsx
@@ -0,0 +1,132 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import Button from '../../components/common/Button.jsx';
+
+const Signup = () => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [message, setMessage] = useState(null);
+	const [messageType, setMessageType] = useState(null);
+	const navigate = useNavigate();
+
+	const MIN_LENGTH = 5;
+	const MAX_LENGTH = 25;
+
+	const submitHandler = async (e) => {
+		e.preventDefault();
+		setMessage('');
+
+		if (!username) {
+			setMessage('Please enter your username.');
+			return;
+		}
+
+		if (username.length < MIN_LENGTH || username.length > MAX_LENGTH) {
+			setMessage('Username must contain 5 to 25 characters.');
+			return;
+		}
+
+		if (!password) {
+			setMessage('Please enter your password.');
+			return;
+		}
+
+		if (password.length < MIN_LENGTH || password.length > MAX_LENGTH) {
+			setMessage('Password must contain 5 to 25 characters.');
+			return;
+		}
+
+		try {
+			const response = await fetch('http://localhost:3000/api/auth/signup', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ username, password }),
+			});
+
+			let data = {};
+
+			try {
+				data = await response.json();
+			} catch {}
+
+			if (!response.ok) {
+				throw new Error(data.message || 'Registration failed');
+			}
+
+			setMessageType('success');
+
+			setMessage('Registration successful! Redirecting to login...');
+
+			setTimeout(() => {
+				navigate('/login');
+			}, 2000);
+		} catch (err) {
+			setMessageType('error');
+			setMessage(err.message);
+		}
+	};
+
+	return (
+		<div className="flex items-center justify-center min-h-screen bg-gray-100">
+			<form
+				onSubmit={submitHandler}
+				className="p-6 bg-white rounded shadow-md w-full max-w-sm"
+			>
+				<img
+					src="/logo-round.png"
+					alt="Sense notes logo"
+					className="flex mb-5 max-h-40 items-center max-w-full mx-auto"
+				/>
+
+				<h2 className="text-2xl font-OpenSans font-bold mb-4 flex flex-row items-center justify-center text-slate-800">
+					Sign up
+				</h2>
+
+				<div className="mb-4">
+					<input
+						type="text"
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+						className="w-full p-2 border border-blue-500 rounded-md mt-1 outline-none font-OpenSans"
+						placeholder="username"
+					/>
+				</div>
+
+				<div className="mb-4">
+					<input
+						type="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						className="w-full p-2 border border-blue-500 rounded-md mt-1 outline-none font-OpenSans"
+						placeholder="password"
+					/>
+				</div>
+
+				{message && (
+					<p
+						className={`mb-4 text-lg font-OpenSans text-center ${
+							messageType === 'success' ? 'text-slate-800' : 'text-red-500'
+						}`}
+					>
+						{message}
+					</p>
+				)}
+
+				<Button className="w-full" type="submit">
+					Enter
+				</Button>
+
+				<p className="mt-4 text-center font-GoogleSans">
+					Already have an account?{' '}
+					<Link to="/login" className="text-blue-500 font-GoogleSans">
+						Log in
+					</Link>
+				</p>
+			</form>
+		</div>
+	);
+};
+
+export default Signup;
