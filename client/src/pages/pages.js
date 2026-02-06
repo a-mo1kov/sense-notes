695940ffea62c3d95cdd7fd4040d6f736948ad2f
a-mo1kov <user.amo1kov07@gmail.com>
2026-02-06 11:04:48 +0300

    chore: centralize pages exports

---
+++ client/src/pages/pages.js
@@ -0,0 +1,8 @@
+import Login from './Auth/Login.jsx';
+import Signup from './Auth/Signup.jsx';
+
+import NotFound from './Error/NotFound.jsx';
+
+import Dashboard from './Notes/Dashboard.jsx';
+
+export { Login, Signup, NotFound, Dashboard };
