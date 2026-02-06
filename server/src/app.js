890c8677aafdd2a71eccc13b6a3312f355f11ebb
a-mo1kov <user.amo1kov07@gmail.com>
2026-02-06 11:04:13 +0300

    chore: enable CORS for frontend origin

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,19 @@
-import express from "express";
-import router from "./routes/router.js";
+import express from 'express';
+import router from './routes/router.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api", router);
+app.use(
+	cors({
+		origin: 'http://localhost:5173',
+		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
+	}),
+);
+
+app.use('/api', router);
 
 export default app;
