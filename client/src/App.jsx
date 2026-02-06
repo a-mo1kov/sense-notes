04b5e80bcd8c939e44a1f5f761cda25d9b347a9b
a-mo1kov <user.amo1kov07@gmail.com>
2026-02-06 11:03:16 +0300

    feat: update application routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Login, Signup, NotFound, Dashboard } from './pages/pages.js';
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route />
+				<Route path="/" element={<Navigate replace to="/login" />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
 	);
